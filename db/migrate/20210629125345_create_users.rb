b026db42a763902d36f68404b3da675f59a9839a
dkliaf <diana.widstrom@gmail.com>
2021-08-13 18:25:06 +0300

    update

--- db/migrate/20210629125345_create_users.rb
+++ db/migrate/20210629125345_create_users.rb
@@ -1,10 +1,10 @@
-class CreateUsers < ActiveRecord::Migration[6.1]
+class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
       t.string :email
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
