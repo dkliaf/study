4d2e3a63a22f89349e4a65456e69b72f5c02ae5d
dkliaf <diana.widstrom@gmail.com>
2021-08-16 22:39:13 +0300

    chapeter_8_log_in_out

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,10 +1,14 @@
-require 'rails_helper'
+require 'test_helper'
 
-RSpec.describe "UsersLogins", type: :request do
-  describe "GET /users_logins" do
-    it "works! (now write some real specs)" do
-      get users_logins_path
-      expect(response).to have_http_status(200)
-    end
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 end
