a06a85cbb82593801cb9ef3f3de207a58e4e0470
dkliaf <diana.widstrom@gmail.com>
2021-08-25 08:55:37 +0200

    unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
@@ -9,8 +8,5 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
-=======
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
->>>>>>> origin/master
